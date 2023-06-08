dce3b2024b409bcf2d1d09330ef471cebaf40706
Souradip Ghosh <84944565+Souradip2000@users.noreply.github.com>
2023-06-08 10:29:16 +0530

    Create June_08.cpp

---
+++ June_08.cpp
@@ -0,0 +1,23 @@
+// 1351. Count Negative numbers in a sorted matrix
+// beats 97.5%
+
+class Solution {
+public:
+    int countNegatives(vector<vector<int>>& grid) {
+        int rows=grid.size();
+        int cols=grid[0].size();
+
+        int count =0;
+
+        for(int i=0; i<rows; i++)
+        {
+            for(int j=0; j<cols; j++)
+            {
+                if(grid[i][j]<0)
+                    count++;
+            }
+        }
+
+        return count;
+    }
+};
