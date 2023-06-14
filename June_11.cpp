fea4835458e870018a42cbf0c475ba2fa97fffc0
KUNNAL BANSAL <79396203+kunna67@users.noreply.github.com>
2023-06-14 09:30:01 +0530

    Update June_11.cpp

--- June_11.cpp
+++ June_11.cpp
@@ -3,30 +3,60 @@
 // memory limit exceeded
 // 69/74 testcases passed
 
-#include <array>
+// #include <array>
+// class SnapshotArray {
+// public:
+//     int sid=0;
+//     vector<int> arr ={0};
+//     vector<vector<int>> snapHis;
+//     SnapshotArray(int length) {
+//         for(int i=0; i<length; i++)
+//         {
+//             arr.push_back(0);
+//         }
+//     }
+    
+//     void set(int index, int val) {
+//         arr[index]=val;
+//     }
+    
+//     int snap() {
+//         snapHis.push_back(arr);
+//         return sid++;
+//     }
+    
+//     int get(int index, int snap_id) {
+//         return snapHis[snap_id][index];
+//     }
+// };
+
+/**
+ * Your SnapshotArray object will be instantiated and called as such:
+ * SnapshotArray* obj = new SnapshotArray(length);
 class SnapshotArray {
 public:
-    int sid=0;
-    vector<int> arr ={0};
-    vector<vector<int>> snapHis;
+  unordered_map<int, map<int, int>> mp;
+    int s = 0;
     SnapshotArray(int length) {
-        for(int i=0; i<length; i++)
-        {
-            arr.push_back(0);
+        for(int i = 0; i < length; i++){
+            map<int, int> tmp;
+            tmp[0] = 0;
+            mp[i] = tmp;
         }
     }
-    
+
     void set(int index, int val) {
-        arr[index]=val;
+        mp[index][s] = val;
     }
     
     int snap() {
-        snapHis.push_back(arr);
-        return sid++;
+        return s++;
     }
     
     int get(int index, int snap_id) {
-        return snapHis[snap_id][index];
+        auto i = mp[index].upper_bound(snap_id); 
+        i--;
+        return i -> second;
     }
 };
 
